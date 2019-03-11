8421affb0fc40e8ea1a24c52c2699979d88ad402
Leo <leoschonberger@gmail.com>
2019-03-11 11:10:52 -0700

    please work
    I did the dice thing

---
+++ Monopoly/Assets/Scripts/RulesAndEnums/DiceFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class DiceFunctions : MonoBehaviour {
+    public int diceOneValue;
+    public int diceTwoValue;
+    public int diceTotal;
+    System.Random rnd = new System.Random();
+
+    public void diceRoll(){
+        Debug.Log("Rolling the dice...");
+        diceOneValue = rnd.Next(0, 6);
+        diceTwoValue = rnd.Next(0, 6);
+    }
+
+    public int getDiceTotalValue(){
+        diceTotal = diceOneValue + diceTwoValue;
+        return diceTotal;
+    }
+    
+}
