66510696431f5d7c33046d3e7c01e6d97c3ce252
Ben <benbridenbaugh@gmail.com>
2019-03-13 12:11:12 -0700

    changed  one last thing

--- Monopoly/Assets/Scripts/GameObjects/Cards/Card.cs
+++ Monopoly/Assets/Scripts/GameObjects/Cards/Card.cs
@@ -1,6 +1,6 @@
 namespace GameObjects.Cards{
     public abstract class Card {
-        protected int Type { get; set; } // type 3 is get out of jail. Type 4 is go to jail
+        public int Type { get; set; } // type 3 is get out of jail. Type 4 is go to jail
         public abstract int Activate(int[] values); //This is for things that change your money based on parameters. type 2
         public abstract int Activate(); //these move you. type 0
         public abstract int Activate(int money); // change your money based on set amount. type 1
