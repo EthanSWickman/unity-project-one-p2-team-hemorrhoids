f6c592ae43a77d552138461c031e7d378b335a20
jeevs <owencook1714@gmail.com>
2019-03-14 10:21:40 -0700

    Maybe?
    need to add stuff so that I can make the array for holding all the board cards. Needs Namespace change for most of the other cards that arent estates

--- Monopoly/Assets/Scripts/RulesAndEnums/HARDCODED/SetBoardArray.cs
+++ Monopoly/Assets/Scripts/RulesAndEnums/HARDCODED/SetBoardArray.cs
@@ -8,8 +8,21 @@ using UnityEngine;
 public class SetBoardArray : MonoBehaviour
 {
     public static GameObjects.Spaces.Space[] SetUpBoardArray(GameObjects.Spaces.Space[] _spaces){
-        if (_spaces != null) {
-            
+        if (_spaces != null)
+        {
+            _spaces[0] = ;
+            _spaces[1] = new EstateSpace(CreateEstatesValueArray(ColorValues.BrownHigh));
+            _spaces[2] = new EstateSpace(CreateEstatesValueArray(ColorValues,BrownLow));
+            _spaces[3]
+            _spaces[4]
+            _spaces[5]
+            _spaces[6]
+            _spaces[7]
+            _spaces[8]
+            _spaces[9]
+            _spaces[10]
+            _spaces[11]
+            _spaces[12]
             _spaces[39] = new EstateSpace(CreateEstatesValueArray(ColorValues.DarkBlueHigh));
         }
     }
