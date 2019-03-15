4b6e254874be96d7568386b2a34c05089305289b
Ben <benbridenbaugh@gmail.com>
2019-03-15 11:32:11 -0700

    Fixed some stuff

--- Monopoly/Assets/Scripts/GameObjects/BoardSpaces/CardSpaces/ChanceSpace.cs
+++ Monopoly/Assets/Scripts/GameObjects/BoardSpaces/CardSpaces/ChanceSpace.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 
 namespace GameObjects.BoardSpaces.CardSpaces {
-    public class ChanceSpace : MonoBehaviour
+    public class ChanceSpace: DrawCardTile
     {
         // Start is called before the first frame update
         void Start()
@@ -13,6 +13,10 @@ namespace GameObjects.BoardSpaces.CardSpaces {
         void Update()
         {
         
+        }
+
+
+        public ChanceSpace(bool chanceOrCommunityChest) : base(chanceOrCommunityChest){
         }
     }
 }
--- Monopoly/Assets/Scripts/GameObjects/Cards/ChanceCards.cs
+++ Monopoly/Assets/Scripts/GameObjects/Cards/ChanceCards.cs
@@ -1,5 +1,15 @@
 namespace GameObjects.Cards {
-    public class ChanceCards {
-        
+    public class ChanceCards : Card{
+        public override int Activate(int[] values){
+            throw new System.NotImplementedException();
+        }
+
+        public override int Activate(){
+            throw new System.NotImplementedException();
+        }
+
+        public override int Activate(int money){
+            throw new System.NotImplementedException();
+        }
     }
 }
